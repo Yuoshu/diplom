7a33c59d1cdd943f74862ca91895ea4c25151672
Yuoshu <91850792+Yuoshu@users.noreply.github.com>
2023-06-08 16:01:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
-// import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 
 
 
@@ -16,8 +16,8 @@ dotenv.config()
 
 connectDB();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
