ffa7a916f38493f9efcef73094479ab2aba0e4c7
Yuoshu <91850792+Yuoshu@users.noreply.github.com>
2023-06-08 16:30:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,15 +32,13 @@ app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product', productRoutes)
 
 
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html");
-  );
+app.use('*', function(req, res){
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
 });
 
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server runnig ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white)  
-}
+    console.log(`Server runnig ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white)
+})
